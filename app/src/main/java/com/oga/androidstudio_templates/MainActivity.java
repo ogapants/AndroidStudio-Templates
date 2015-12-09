2be409493170bc67e7c905722e3a52291cf73c2a
ogapants <ogapamyu@gmail.com>
2015-12-09 20:26:31 +0900

    Add EmptyActivity.java

--- app/src/main/java/com/oga/androidstudio_templates/MainActivity.java
+++ app/src/main/java/com/oga/androidstudio_templates/MainActivity.java
@@ -1,13 +1,43 @@
 package com.oga.androidstudio_templates;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.oga.androidstudio_templates.activities.EmptyActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final List<Class<EmptyActivity>> classes = Arrays.asList(
+            EmptyActivity.class
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> list = new ArrayList<>();
+        for (Class<? extends Activity> aClass : classes) {
+            list.add(aClass.getSimpleName() + ".java");
+        }
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(MainActivity.this, classes.get(position)));
+            }
+        });
     }
 }
