5a5a7c41a4f1e01968f42293b2cac18f4f07d9aa
ogapants <ogapamyu@gmail.com>
2016-04-25 01:00:47 +0900

    Update to Android Studio 2.0

--- app/src/main/java/com/oga/androidstudio_templates/MainActivity.java
+++ app/src/main/java/com/oga/androidstudio_templates/MainActivity.java
@@ -1,73 +1,13 @@
 package com.oga.androidstudio_templates;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.oga.androidstudio_templates.activities.AdMobActivity;
-import com.oga.androidstudio_templates.activities.BlankActivity;
-import com.oga.androidstudio_templates.activities.BlankHierarchicalActivity;
-import com.oga.androidstudio_templates.activities.BlankUseFragmentActivity;
-import com.oga.androidstudio_templates.activities.EmptyActivity;
-import com.oga.androidstudio_templates.activities.FullscreenActivity;
-import com.oga.androidstudio_templates.activities.ItemDetailActivity;
-import com.oga.androidstudio_templates.activities.ItemListActivity;
-import com.oga.androidstudio_templates.activities.LoginActivity;
-import com.oga.androidstudio_templates.activities.MapsActivity;
-import com.oga.androidstudio_templates.activities.NavigationDrawerActivity;
-import com.oga.androidstudio_templates.activities.ScrollingActivity;
-import com.oga.androidstudio_templates.activities.SettingsActivity;
-import com.oga.androidstudio_templates.activities.TabbedSpinnerActivity;
-import com.oga.androidstudio_templates.activities.TabbedSwipeActivity;
-import com.oga.androidstudio_templates.activities.TabbedTabsActivity;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final List<Class<? extends Activity>> classes = Arrays.asList(
-            AdMobActivity.class,
-            BlankActivity.class,
-            BlankHierarchicalActivity.class,
-            BlankUseFragmentActivity.class,
-            EmptyActivity.class,
-            FullscreenActivity.class,
-            ItemDetailActivity.class,
-            ItemListActivity.class,
-            LoginActivity.class,
-            MapsActivity.class,
-            NavigationDrawerActivity.class,
-            ScrollingActivity.class,
-            SettingsActivity.class,
-            TabbedSpinnerActivity.class,
-            TabbedSwipeActivity.class,
-            TabbedTabsActivity.class
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        List<String> list = new ArrayList<>();
-        for (Class<? extends Activity> aClass : classes) {
-            list.add(aClass.getSimpleName() + ".java");
-        }
-
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list));
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(MainActivity.this, classes.get(position)));
-            }
-        });
     }
 }
